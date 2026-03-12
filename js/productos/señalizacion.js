4dd9378b445f457e6a41c2d9665eda9ad15b2ac7
Mateo Miranda <mateojmiranda04@gmail.com>
2026-03-12 11:38:27 -0300

    Carga señalización completa

--- js/productos/señalizacion.js
+++ js/productos/señalizacion.js
@@ -102,4 +102,293 @@ const señalizacion = [
         {nombre: "Verde f", imagenes: ["imagenes/Productos/señalizacion/chalecopesadoverde.png"]}
     ]
   },
+  {
+    id: "señalizacion-8",
+    nombre: "Bandolera c/ reflectivo continuo 1cm",
+    Descripcion: "Se confecciona con cortes de cinta 25x10 m/m de transfer. En color naranja o amarillo todo su cuerpo principal. En el frente cuenta con una cinta combinada reflectiva en color amarilla o naranja de 55 m/m. dependiendo del color de la bandolera, base de poliÃ©ster de 25 m/m con su centro de 10 mm de reflectivo.\nEn un largo aplicado de 20 cm. sobre el centro de la bandolera repitiendo su proceso sobre la parte trasera de la misma. Cantidad (2) horizontales delantero y trasero y (4) cruzados dos delantero pecho y dos traseros pecho de un largo de 170 m/m cada corte. Laterales (2) elasticos 230 M/M de largo x 55 M/M de alto.",
+    Especificaciones: "Cinta textil: 55 mm. 100 % poliéster\nCinta textil: 25 mm. con reflectivo 10 mm\nElástico: 60% polyester y 40% goma\nColores: Naranja / Amarillo\nReflectivo: 500 candelas lux",
+    Documentacion: [
+    { nombre: "No disponible"}
+    ],
+    categoria: "Señalización",
+    subcategoria: "Indumentaria",  
+    marca: "MAXIMO ESFUERZO",
+    destacado: false,
+    colores:[
+        {nombre: "Naranja f", imagenes: ["imagenes/Productos/señalizacion/bandonaranja.png"]},
+        {nombre: "Verde f", imagenes: ["imagenes/Productos/señalizacion/bandoverde.png"]}
+    ]
+  },
+  {
+    id: "señalizacion-9",
+    nombre: "Cinta 'Peligro' roja/blanca (8cm x 200m)",
+    Descripcion: "Cinta de demarcación con leyenda 'Peligro'",
+    Especificaciones: "Ancho: 8cm\n Largo: 200m",
+    Documentacion: [
+    { nombre: "No disponible"}
+    ],
+    categoria: "Señalización",
+    subcategoria: "Demarcación",  
+    marca: "",
+    destacado: false,
+    imagenes:[
+      "imagenes/Productos/señalizacion/cintapeligro.png"
+    ]
+  },
+  {
+    id: "señalizacion-10",
+    nombre: "Cinta 'Precaución' amarilla/negra (200m)",
+    Descripcion: "Cinta de demarcación con leyenda 'Precaución'",
+    Especificaciones: "Largo: 200m",
+    Documentacion: [
+    { nombre: "No disponible"}
+    ],
+    categoria: "Señalización",
+    subcategoria: "Demarcación",  
+    marca: "",
+    destacado: false,
+    imagenes:[
+      "imagenes/Productos/señalizacion/cintaprecacuion.png"
+    ]
+  },
+  {
+    id: "señalizacion-11",
+    nombre: "Cadena Plástica blanca/roja (6x29x49)",
+    Descripcion: "Cadena plástica de demarcación blanca y roja.",
+    Especificaciones: "Material: Plástico.",
+    Documentacion: [
+    { nombre: "No disponible"}
+    ],
+    categoria: "Señalización",
+    subcategoria: "Demarcación",  
+    marca: "",
+    destacado: false,
+    imagenes:[
+      "imagenes/Productos/señalizacion/cadenaroja.png"
+    ]
+  },
+  {
+    id: "señalizacion-12",
+    nombre: "Cadena Plástica amarilla/negra (6x29x49)",
+    Descripcion: "Cadena plástica de demarcación amarilla y negra.",
+    Especificaciones: "Material: Plástico.",
+    Documentacion: [
+    { nombre: "No disponible"}
+    ],
+    categoria: "Señalización",
+    subcategoria: "Demarcación",  
+    marca: "",
+    destacado: false,
+    imagenes:[
+      "imagenes/Productos/señalizacion/cadenaamarilla.png"
+    ]
+  },
+  {
+    id: "señalizacion-13",
+    nombre: "Bandera cebrada roja/blanca (50cm x 70cm)",
+    Descripcion: "Bandera de tela cebrada roja y blanca",
+    Especificaciones: "Material: Tela\nAlto: 50cm\nLargo: 70cm",
+    Documentacion: [
+    { nombre: "No disponible"}
+    ],
+    categoria: "Señalización",
+    subcategoria: "Demarcación",  
+    marca: "",
+    destacado: false,
+    imagenes:[
+      "imagenes/Productos/señalizacion/banderarojayb.png"
+    ]
+  },
+  {
+    id: "señalizacion-14",
+    nombre: "Bandera cebrada roja/blanca c/ inscripción 'PELIGRO' (50cm x 70cm)",
+    Descripcion: "Bandera de tela cebrada roja y blanca con inscripción 'PELIGRO'",
+    Especificaciones: "Material: Tela\nAlto: 50cm\nLargo: 70cm",
+    Documentacion: [
+    { nombre: "No disponible"}
+    ],
+    categoria: "Señalización",
+    subcategoria: "Demarcación",  
+    marca: "",
+    destacado: false,
+    imagenes:[
+      "imagenes/Productos/señalizacion/banderapeligro.png"
+    ]
+  },
+  {
+    id: "señalizacion-15",
+    nombre: "Malla plástica de seguridad 1m x 45m (80g/m²)",
+    Descripcion: "Malla de seguridad plástica NARANJA.",
+    Especificaciones: "Material: plástico\nAlto: 1m\nLargo: 45m\nDensidad: 80g/m²",
+    Documentacion: [
+    { nombre: "No disponible"}
+    ],
+    categoria: "Señalización",
+    subcategoria: "Demarcación",  
+    marca: "",
+    destacado: false,
+    imagenes:[
+      "imagenes/Productos/señalizacion/malla.png"
+    ]
+  },
+  {
+    id: "señalizacion-16",
+    nombre: "Bastón luminoso 41cm LED a pila (Fija / Intermitente)",
+    Descripcion: "Son dispositivos de señalización visual ideales para las operaciones en Puestos de Control Vial, retenes, desviaciones en vías, parqueaderos, señales de auxilio y aeropuertos, debido a su funciones fija o intermitente de luz color roja.\nLa función señalizadora es de vital importancia a la hora de advertir una situación y evitar posibles accidentes.\nEsta baliza, es ideal para usar en días lluviosos o con neblina, perfecto para situaciones de emergencia. La cubierta protectora permite su fácil mantenimiento.\nFácil de usar y es ideal para casos de emergencia.",
+    Especificaciones: "Material: plástico\nLargo: 41cm.\nPILAS NO INCLUIDAS",
+    Documentacion: [
+    { nombre: "No disponible"}
+    ],
+    categoria: "Señalización",
+    subcategoria: "Demarcación",  
+    marca: "STRONG SAFETY",
+    destacado: false,
+    imagenes:[
+      "imagenes/Productos/señalizacion/baston41cm.png"
+    ]
+  },
+  {
+    id: "señalizacion-17",
+    nombre: "Bastón luminoso 55cm LED recargable USB/200V (Fija / Intermitente)",
+    Descripcion: "Son dispositivos de señalización visual ideales para las operaciones en Puestos de Control Vial, retenes, desviaciones en vías, parqueaderos, señales de auxilio y aeropuertos, debido a su funciones fija o intermitente de luz color roja.\nLa función señalizadora es de vital importancia a la hora de advertir una situación y evitar posibles accidentes.\nEsta baliza, es ideal para usar en días lluviosos o con neblina, perfecto para situaciones de emergencia. La cubierta protectora permite su fácil mantenimiento.\nFácil de usar y es ideal para casos de emergencia.",
+    Especificaciones: "Material: plástico\nLargo: 55cm.\nRecargable: SI (USB/220V)",
+    Documentacion: [
+    { nombre: "No disponible"}
+    ],
+    categoria: "Señalización",
+    subcategoria: "Demarcación",  
+    marca: "STRONG SAFETY",
+    destacado: false,
+    imagenes:[
+      "imagenes/Productos/señalizacion/baston55cm.png"
+    ]
+  },
+  {
+    id: "señalizacion-18",
+    nombre: "Cinta reflectiva Roja/Blanca (45,7m x 75mm)",
+    Descripcion: "Se trata de un rollo de lámina microprismática de colores rojo y blanco altamente reflectante, con un adhesivo sensible a la presión que facilita su aplicación en camiones, remolques y vehículos grandes para mejorar la visibilidad y la distancia de detección.",
+    Especificaciones: "Largo: 45,7m\n Ancho: 75mm ",
+    Documentacion: [
+    { nombre: "No disponible"}
+    ],
+    categoria: "Señalización",
+    subcategoria: "Vehicular",  
+    marca: "3M",
+    destacado: false,
+    imagenes:[
+      "imagenes/Productos/señalizacion/cintareflecryb.png"
+    ]
+  },
+  {
+    id: "señalizacion-19",
+    nombre: "Cinta reflectiva Roja (45,7m x 75mm)",
+    Descripcion: "Banda perimetral altamente reflectante con adhesivo ultra resistente sensible a la presión para una fácil aplicación en camiones. remolques y vehículos grandes para mejorar la visibilidad y la distancia de detección.",
+    Especificaciones: "Largo: 45,7m\n Ancho: 75mm ",
+    Documentacion: [
+    { nombre: "No disponible"}
+    ],
+    categoria: "Señalización",
+    subcategoria: "Vehicular",  
+    marca: "3M",
+    destacado: false,
+    imagenes:[
+      "imagenes/Productos/señalizacion/cintareflecroja.png"
+    ]
+  },
+    {
+    id: "señalizacion-20",
+    nombre: "Cinta reflectiva Blanca (45,7m x 75mm)",
+    Descripcion: "Banda perimetral altamente reflectante con adhesivo ultra resistente sensible a la presión para una fácil aplicación en camiones. remolques y vehículos grandes para mejorar la visibilidad y la distancia de detección.",
+    Especificaciones: "Largo: 45,7m\n Ancho: 75mm ",
+    Documentacion: [
+    { nombre: "No disponible"}
+    ],
+    categoria: "Señalización",
+    subcategoria: "Vehicular",  
+    marca: "3M",
+    destacado: false,
+    imagenes:[
+      "imagenes/Productos/señalizacion/cintareflecblanca.png"
+    ]
+  },
+    {
+    id: "señalizacion-21",
+    nombre: "Círculo reflectivo velocidad máxima 90km/h",
+    Descripcion: "Círculo reflectivo para señalizar velocidad máxima del vehículo.",
+    Especificaciones: "Dimensiones estándar.",
+    Documentacion: [
+    { nombre: "No disponible"}
+    ],
+    categoria: "Señalización",
+    subcategoria: "Vehicular",  
+    marca: "3M",
+    destacado: false,
+    imagenes:[
+      "imagenes/Productos/señalizacion/90km.png"
+    ]
+  },
+  {
+    id: "señalizacion-22",
+    nombre: "Círculo reflectivo velocidad máxima 80km/h",
+    Descripcion: "Círculo reflectivo para señalizar velocidad máxima del vehículo.",
+    Especificaciones: "Dimensiones estándar.",
+    Documentacion: [
+    { nombre: "No disponible"}
+    ],
+    categoria: "Señalización",
+    subcategoria: "Vehicular",  
+    marca: "3M",
+    destacado: false,
+    imagenes:[
+      "imagenes/Productos/señalizacion/80km.png"
+    ]
+  },
+  {
+    id: "señalizacion-23",
+    nombre: "Círculo reflectivo velocidad máxima 110km/h",
+    Descripcion: "Círculo reflectivo para señalizar velocidad máxima del vehículo.",
+    Especificaciones: "Dimensiones estándar.",
+    Documentacion: [
+    { nombre: "No disponible"}
+    ],
+    categoria: "Señalización",
+    subcategoria: "Vehicular",  
+    marca: "3M",
+    destacado: false,
+    imagenes:[
+      "imagenes/Productos/señalizacion/110km.png"
+    ]
+  },
+  {
+    id: "señalizacion-24",
+    nombre: "Baliza triangular económica con estuche y base plástica (x2 un.)",
+    Descripcion: "Par de balizas triangulares, con estuche que sirve como base.",
+    Especificaciones: "Dimensiones estándar.",
+    Documentacion: [
+    { nombre: "No disponible"}
+    ],
+    categoria: "Señalización",
+    subcategoria: "Vehicular",  
+    marca: "",
+    destacado: false,
+    imagenes:[
+      "imagenes/Productos/señalizacion/balizas.png"
+    ]
+  },
+  {
+    id: "señalizacion-25",
+    nombre: "Kit para automóvil bolso (Botiquín - Extintor - Baliza - Chaleco)",
+    Descripcion: "El kit contiene:\nBotiquín económico PRACTY BOTIQUÍN\nExtintor YUKON 1kg (variante depende de disponibilidad)\nBaliza triangular\nChaleco con 4 reflectivos.",
+    Especificaciones: "Ver detalle en productos individuales.",
+    Documentacion: [
+    { nombre: "No disponible"}
+    ],
+    categoria: "Señalización",
+    subcategoria: "Vehicular",  
+    marca: "",
+    destacado: false,
+    imagenes:[
+      "imagenes/Productos/señalizacion/kit.png","imagenes/Productos/insumos/botiquines/100.png","imagenes/Productos/señalizacion/balizas.png","imagenes/Productos/señalizacion/chalecopesadonaranja.png","imagenes/Productos/insumos/extintores/1kg1.png"
+    ]
+  },
 ]
